f87957344fd47837ff10bee97049b27322aaeaef
Alex Koryakov <31795607+DoomzD@users.noreply.github.com>
2018-03-28 13:54:01 +0300

    Minor upgrades

--- HashMap.h
+++ HashMap.h
@@ -9,6 +9,9 @@ using std::pair;
 using std::list;
 using std::vector;
 
+const double MAX_LOAD_FACTOR = 2. / 3.;
+const double MIN_LOAD_FACTOR = 1. / 4.;
+
 template<class KeyType, class ValueType, class Hash = std::hash<KeyType>>
 class HashMap {
 public:
@@ -17,28 +20,57 @@ public:
     using iterator = typename list<value_type>::iterator;
     using const_iterator = typename list<value_type>::const_iterator;
 
+    friend void swap(HashMap& lhs, HashMap& rhs) {
+        using std::swap;
+
+        swap(lhs.hasher_, rhs.hasher_);
+        swap(lhs.table_, rhs.table_);
+        swap(lhs.ind_, rhs.ind_);
+        swap(lhs.used_, rhs.used_);
+        swap(lhs.erased_, rhs.erased_);
+        swap(lhs.size_, rhs.size_);
+        swap(lhs.buckets_, rhs.buckets_);
+    }
+
     HashMap(const Hash& hasher = Hash())
-            : hasher_(hasher), size_(0), buckets_(2) {
+            : hasher_(hasher), size_(0), buckets_(1) {
         rebuild(buckets_);
+    }
+
+    HashMap(const HashMap<KeyType, ValueType, Hash>& other)
+            : table_(other.table_) {
+        hasher_ = other.hasher_;
+        ind_ = other.ind_;
+        used_ = other.used_;
+        erased_ = other.erased_;
+        size_ = other.size_;
+        buckets_ = other.buckets_;
+    }
+
+    HashMap(HashMap<KeyType, ValueType, Hash>&& other)
+            : HashMap() {
+        swap(*this, other);
     }
 
     template<typename ForwardIterator>
     HashMap(ForwardIterator begin, ForwardIterator end, const Hash& hasher = Hash())
-            : hasher_(hasher), size_(0), buckets_(2) {
+            : hasher_(hasher), size_(0), buckets_(1) {
         while (begin != end) {
-            insert_(*begin++);
+            insert(*begin++);
         }
     }
 
-    using InitializerIterator = typename std::initializer_list<value_type>::iterator;
     HashMap(const std::initializer_list<value_type>& init_list, const Hash& hasher = Hash())
-            : hasher_(hasher), size_(0), buckets_(2) {
-        InitializerIterator begin = init_list.begin();
-        InitializerIterator end = init_list.end();
+            : HashMap(init_list.begin(), init_list.end(), hasher) {}
 
-        while (begin != end) {
-            insert_(*begin++);
-        }
+    HashMap& operator = (HashMap<KeyType, ValueType, Hash> other) {
+        swap(*this, other);
+        return *this;
+    }
+
+    HashMap& operator = (HashMap<KeyType, ValueType, Hash>&& other) {
+        swap(*this, other);
+        return *this;
     }
 
     void insert(value_type elem) {
@@ -55,7 +87,7 @@ public:
         ind_[pos] = --table_.end();
         ++size_;
 
-        if (size_ > buckets_ * 2 / 3) {
+        if (size_ > buckets_ * MAX_LOAD_FACTOR) {
             rebuild(buckets_ * 2);
         }
     }
@@ -73,7 +105,7 @@ public:
         erased_[pos] = true;
         --size_;
 
-        if (size_ < buckets_ / 4) {
+        if (size_ < buckets_ * MIN_LOAD_FACTOR) {
             rebuild(buckets_ / 2);
         }
     }
@@ -115,39 +147,20 @@ public:
     }
 
     ValueType& operator [] (const KeyType& key) {
-        size_t pos = hasher_(key) % buckets_;
-        while ((used_[pos] && !(ind_[pos]->first == key)) || erased_[pos]) {
-            pos = (pos + 1 == buckets_ ? 0 : pos + 1);
+        iterator found = find(key);
+        if (found != end()) {
+            return found->second;
         }
 
-        if (used_[pos]) return ind_[pos]->second;
-
-        pair<KeyType, ValueType> temp;
-        temp.first = key;
-        value_type newElem(temp);
-        table_.emplace_back(newElem);
-        ind_[pos] = --table_.end();
-        used_[pos] = true;
-
-        ++size_;
-        if (size_ > buckets_ * 2 / 3) {
-            rebuild(buckets_ * 2);
-            pos = hasher_(key) % buckets_;
-            while ((used_[pos] && !(ind_[pos]->first == key)) || erased_[pos]) {
-                pos = (pos + 1 == buckets_ ? 0 : pos + 1);
-            }
-        }
-
-        return ind_[pos]->second;
+        insert({key, ValueType()});
+        found = find(key);
+        return found->second;
     }
 
     const ValueType& at(const KeyType& key) const {
-        size_t pos = hasher_(key) % buckets_;
-        while ((used_[pos] && !(ind_[pos]->first == key)) || erased_[pos]) {
-            pos = (pos + 1 == buckets_ ? 0 : pos + 1);
-        }
-
-        if (used_[pos]) return ind_[pos]->second;
+        const_iterator found = find(key);
+        if (found != end())
+            return found->second;
         throw std::out_of_range("no such element");
     }
 
@@ -156,37 +169,6 @@ public:
         size_ = 0;
         buckets_ = 2;
         rebuild(buckets_);
-    }
-
-    HashMap& operator = (const HashMap& other) {
-        if (this == &other) return *this;
-        table_.clear();
-        buckets_ = other.buckets_;
-        size_ = other.size_;
-        rebuild(buckets_);
-
-        auto it = other.begin();
-        while (it != other.end()) {
-            table_.emplace_back(*it++);
-            size_t pos = hasher_(table_.back().first) % buckets_;
-            while (used_[pos]) {
-                pos = (pos + 1 == buckets_ ? 0 : pos + 1);
-            }
-            ind_[pos] = --table_.end();
-            used_[pos] = true;
-        }
-
-        return *this;
-    }
-
-    hash_table& operator = (const hash_table& other) {
-        this->clear();
-        auto it = other.begin();
-        while (it != other.end()) {
-            table_.emplace_back(*it++);
-        }
-
-        return table_;
     }
 
     size_t size() const {
@@ -203,8 +185,7 @@ public:
 
 private:
     void rebuild(unsigned int sz) {
-//        cout << size_ << ":" << buckets_ << '\n';
-        hash_table temp = table_;
+        hash_table temp(table_);
         table_.clear();
         buckets_ = sz;
         vector<iterator> newInd(buckets_, table_.end());
@@ -226,29 +207,11 @@ private:
         erased_ = newErased;
     }
 
-    void insert_(value_type elem) {
-        size_t pos = hasher_(elem.first) % buckets_;
-        while ((used_[pos] && !(ind_[pos]->first == elem.first)) || erased_[pos]) {
-            pos = (pos + 1 == buckets_ ? 0 : pos + 1);
-        }
-
-        used_[pos] = true;
-        erased_[pos] = false;
-
-        table_.emplace_back(elem);
-        ind_[pos] = --table_.end();
-        ++size_;
-
-        if (size_ > buckets_ * 2 / 3) {
-            rebuild(buckets_ * 2);
-        }
-    }
-
     Hash hasher_;
     hash_table table_;
-    vector<iterator> ind_ = {table_.end(), table_.end()};
-    vector<bool> used_ = {false, false};
-    vector<bool> erased_ = {false, false};
+    vector<iterator> ind_ = {table_.end()};
+    vector<bool> used_ = {false};
+    vector<bool> erased_ = {false};
     size_t size_;
     unsigned int buckets_;
 };
